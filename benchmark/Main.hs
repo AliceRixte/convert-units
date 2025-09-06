12918f1054e3657493969426fa265057b57f1ffd
Alice Rixte <alice.rixte@gmail.com>
2025-09-06 19:33:32 +0200

    Fix benchmark

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -12,28 +12,32 @@ import Criterion.Main
 import Data.Units
 import Data.Units.NonStd.Time
 
--- mkBench f n = bench (show n) $ nf f n
+mkBench f n = bench (show n) $ nf f n
 
--- iterations :: [Int]
--- iterations = [10, 100, 1000]
-
-
--- kmphTomps :: Int -> Double
--- kmphTomps n =  iterate (fromToCoerce @(Kilo Meter ./. Hour) @(Meter ./. Second)) 5 !! n
-
--- multConv :: Int -> Double
--- multConv n =  iterate (*3.6) (5 :: Double) !! n
+iterations :: [Int]
+iterations = [10, 100, 1000]
 
 
+kmphTomps :: Int -> Double
+kmphTomps n =  iterate
+  (unQuantity . fromTo @(Kilo Meter ./. Hour) @(Meter ./. Second) . quantity)
+  5 !! n
+
+multConv :: Int -> Double
+multConv n =  iterate (*3.6) (5 :: Double) !! n
+
+-- | Check ddump to see it inlines correctly
 u :: (.^-) Second 1 Double
 u = fromTo (1415 :: Hertz Double)
 
+-- main :: IO()
+-- main = print u
+
 main :: IO()
-main = print u
--- main = do
---   defaultMain [
---     bgroup "~>" [
---         bgroup "kmph ~> mps" (fmap (mkBench kmphTomps) iterations)
---       , bgroup "mult" (fmap (mkBench multConv) iterations)
---       ]
---     ]
+main = do
+  defaultMain [
+    bgroup "~>" [
+        bgroup "kmph ~> mps" (fmap (mkBench kmphTomps) iterations)
+      , bgroup "mult" (fmap (mkBench multConv) iterations)
+      ]
+    ]
