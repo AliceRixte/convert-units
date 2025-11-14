fe57dcbd6ffcf53f1a4b6f1591b9da677f079e90
Alice Rixte <alice.rixte@u-bordeaux.fr>
2025-11-14 16:19:31 +0100

    Avoid warnings for imperial area units

--- src/Data/Units/Imperial/Area.hs
+++ src/Data/Units/Imperial/Area.hs
@@ -9,12 +9,12 @@ import Data.Units.SI
 
 -- | Area in acres.
 --
-$(mkUnit "Acre" "ac" ''Area $ (1200 / 3937)^2 * 43560)
+$(mkUnit "Acre" "ac" ''Area $ (1200 / 3937)^ (2 :: Integer) * 43560)
 
 -- | Area in perches.
 --
-$(mkUnit "Perch" "perch" ''Area $ (1200 / 3937)^2 * 1089 / 4)
+$(mkUnit "Perch" "perch" ''Area $ (1200 / 3937)^ (2 :: Integer) * 1089 / 4)
 
 -- | Area in roods.
 --
-$(mkUnit "Rood" "ro" ''Area $ (1200 / 3937)^2 * 10890)
+$(mkUnit "Rood" "ro" ''Area $ (1200 / 3937)^ (2 :: Integer) * 10890)
